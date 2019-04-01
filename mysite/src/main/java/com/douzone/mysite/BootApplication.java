cf8a208282fdcb238088d615fe1fbde37381824b
BIT <BIT@DESKTOP-9NKNPSR>
2019-04-01 16:45:24 +0900

    slack-tomcat test

--- mysite/src/main/java/com/douzone/mysite/BootApplication.java
+++ mysite/src/main/java/com/douzone/mysite/BootApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 //자식 스캔
 public class BootApplication {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
